fdbb79460b5d66d5c0b179197e4012d41dd2cc8d
erwinschoolzilla <erwin@schoolzilla.com>
2015-11-30 21:34:42 -0500

    Updated for DEV

--- src/main/groovy/com/erwindev/controller/HelloController.groovy
+++ src/main/groovy/com/erwindev/controller/HelloController.groovy
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot! (DEV)"
+        return "Greetings from Spring Boot! (DEV - hell to the yeah!)"
     }
 
 }
