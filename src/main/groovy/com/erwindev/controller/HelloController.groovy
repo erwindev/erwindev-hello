dee2796c501e6ef6ac125544bb7be6af32ef1b53
erwinschoolzilla <erwin@schoolzilla.com>
2015-11-30 22:27:27 -0500

    Updated for PROD

--- src/main/groovy/com/erwindev/controller/HelloController.groovy
+++ src/main/groovy/com/erwindev/controller/HelloController.groovy
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot! (PROD)"
+        return "Greetings from Spring Boot! (PRODUCTION BABY!!)"
     }
 
 }
