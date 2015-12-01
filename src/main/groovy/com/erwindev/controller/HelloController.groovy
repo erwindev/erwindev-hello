3d6bd3c829933225f46313970700dd8a3462b347
erwinschoolzilla <erwin@schoolzilla.com>
2015-11-30 22:00:15 -0500

    Updated for UAT

--- src/main/groovy/com/erwindev/controller/HelloController.groovy
+++ src/main/groovy/com/erwindev/controller/HelloController.groovy
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot! (UAT)"
+        return "Greetings from Spring Boot! (UAT - try this one!)"
     }
 
 }
